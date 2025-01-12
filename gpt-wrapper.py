03b86b548ec8af92f46c81eddc4ef11ce3a8e466
Jingyi-Guo <alisa.jingyi.guo@gmail.com>
2025-01-12 18:37:59 -0500

    getting it working

---
+++ gpt-wrapper.py
@@ -0,0 +1,18 @@
+#This file will serve as our GPT connection
+
+from openai import OpenAI
+import streamlit as st
+
+
+api_key = st.secrets["api_key"]
+client = OpenAI(api_key = api_key)
+
+def generate_text(prompt):
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": "write a haiku about ai"}
+        ]
+    )
+
+    print(completion.choices[0].message.content)
